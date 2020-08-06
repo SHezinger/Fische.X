b3b68300e0e41b694255e5bc2e890343c8724a37
SHezinger <SHezinger@BYPKN13.local>
2020-08-06 15:52:48 +0200

    PIC12LF1571 :)

--- mcc_generated_files/interrupt_manager.h
+++ mcc_generated_files/interrupt_manager.h
@@ -16,7 +16,7 @@
     all modules selected in the GUI.
     Generation Information :
         Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.78.1
-        Device            :  PIC12F1571
+        Device            :  PIC12LF1571
         Driver Version    :  2.03
     The generated drivers are tested against the following:
         Compiler          :  XC8 2.10 and above or later
